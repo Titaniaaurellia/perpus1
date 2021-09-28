54cd1122e8e7ee610bf411da99d4516cce1fb8b8
Titaniaaurellia <73207205+Titaniaaurellia@users.noreply.github.com>
2021-09-28 10:28:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<html lang="en">
+<head>
+  <title>Data Anggota Perpustakaan</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_siswa.php">Data Siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php">Data Kelas</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container" text align = "center">
+  <h3>Selamat Datang di Web Data Anggota Perpustakaan</h3><br>
+  <img src = "perpus.jpg" height = "20%" widht = "20%"><br>
+  <table border = "0" align = "center">
+                <tr>
+                    <td>Step 1:</td>
+                    <td>Pilih button Tambah Siswa. Kemudian isi seluruh tabel yang ada sesuai yang diinginkan.</td>
+                </tr>
+                <tr>
+                    <td>Step 2:</td>
+                    <td>Pilih button Tambah Siswa. Kemudian isi seluruh tabel yang ada sesuai yang diinginkan.</td>
+                </tr>
+                <tr>
+                    <td>Step 3:</td>
+                    <td>Pilih button Tambah Siswa. Kemudian isi seluruh tabel yang ada sesuai yang diinginkan.</td>
+                </tr> 
+                <tr>
+                    <td>Step 4:</td>
+                    <td>Kemudian isi tabel sesuai kelas dan kelompokmu. Setelah diisi klik button tambah kelas</td>
+                </tr>
+                <tr>
+                    <td>Step 5:</td>
+                    <td>Setelah kelasmu tersedia, kembali ke navbar Tambah Siswa dan isi seperti tadi.</td>
+                </tr>
+                <tr>
+                    <td>Step 6:</td>
+                    <td>Pastikan datamu sudah masuk dengan mengeceknya di Data Siswa.</td>
+                </tr>
+                <tr>
+                    <td>Step 7:</td>
+                    <td>Selamat kamu telah menjadi anggota perpustakaan.</td>
+                </tr> 
+  <p>Di web ini kami akan menampilkan data dari para anggota perpustakaan sekolah kita. 
+    Jika kalian ingin menjadi anggota dari perpustakaan, kalian bisa mengisi datanya juga. Berikut caranya :</p>
+</div>
+
+</body>
+</html>
